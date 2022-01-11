3ad00f7a7a0afe847608a180c7d46bfc73a5f065
Vishnu V <84262256+VISHNU02V@users.noreply.github.com>
2022-01-11 21:15:47 +0530

    Add files via upload

---
+++ postfix.c
@@ -0,0 +1,82 @@
+#include<stdio.h>
+#include<ctype.h>
+
+char stack[50];
+int top=-1;
+void push(char x){
+    top=top+1;
+    stack[top]=x;
+}
+char pop(){
+    char x;
+    x=stack[top];
+    top=top-1;
+    return x;
+}
+int pri(char symbol){
+    if(symbol=='^'){
+        return (6);
+
+    }
+    else if (symbol=='*'||symbol=='/')
+    {
+        return (3);
+    }
+    else if (symbol=='+'||symbol=='-')
+    {
+        return (1);
+    }
+    else{
+        return (0);
+    }
+}
+int prs(char symbol){
+    if (symbol=='^')
+    {
+        return (5);
+    }
+    else if (symbol=='*'||symbol=='/')
+    {
+        return (4);
+    }
+    else if (symbol=='+'||symbol=='-')
+    {
+        return (2);
+    }
+    else{
+        return (0);
+    }
+    
+}
+void main(){
+    char infix[50],postfix[50],ch,elem;
+    int i=0,k=0;
+    printf("/////////    TO CONVERT INFIX TO POSTFIX     //////////");
+    printf("\ninputing......\nenter the infix expression :");
+    scanf("%s",infix);
+    push('#');
+    while ( (ch=infix[i])!='\0')
+    {
+        if(ch=='('){
+            push(ch);
+        }
+        else if(isalnum(ch)){
+            postfix[k++]=ch;
+        }
+        else if(ch==')'){
+            while(stack[top]!='(')
+                postfix[k++]=pop();
+                elem=pop();
+        }
+        else{
+            while(pri(ch)<prs(stack[top]))
+                postfix[k++]=pop();
+                push(ch); 
+        }
+        i++;
+    }
+    while(stack[top]!='#')
+        postfix[k++]=pop();
+        postfix[k]='\0';
+        printf("\nOUTPUT\nthe postfix expression is: %s",postfix);   
+}
